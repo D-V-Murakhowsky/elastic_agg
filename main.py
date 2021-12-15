05d8dd675fdbfea0f579996cbdf6065a5b80eb0f
DenysMurakhovskyi <dmurakhovskyi@protonmail.com>
2021-12-15 11:10:28 +0200

    typing upd

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from elasticsearch import Elasticsearch
 import logging
 import pandas as pd
+from pandas import DataFrame
 from random import choices
 from dataclasses import dataclass, asdict
+from typing import Dict, Any
 
 CONTENT_FEATURES = ['Wine', 'Producer', 'Denomination']
 
-logger = logging.getLogger('main_logger')
+logger: logging.Logger = logging.getLogger('main_logger')
 
 
 @dataclass
@@ -22,7 +24,7 @@ class ContentElement:
 
 class ESOperations:
     try:
-        es = Elasticsearch(hosts='localhost:9200', timeout=300)
+        es: Elasticsearch = Elasticsearch(hosts='localhost:9200', timeout=300)
         logger.info('Elastic search connected')
     except Exception as ex:
         es = None
@@ -31,24 +33,24 @@ class ESOperations:
 
     def __init__(self):
         self.sample_content = []
-        df = pd.read_pickle('sample_content.pickle')
+        df: DataFrame = pd.read_pickle('sample_content.pickle')
         for col in df.columns:
             self.sample_content.extend(list(df[col]))
 
     def make_and_fill_sample_index(self):
         self.create_sample_index()
         for _ in range(50):
-            cont = choices(self.sample_content, k=3)
-            doc = ContentElement(Wine=cont[0],
-                                 Producer=cont[1],
-                                 Denomination=cont[2])
+            cont: list = choices(self.sample_content, k=3)
+            doc: ContentElement = ContentElement(Wine=cont[0],
+                                                 Producer=cont[1],
+                                                 Denomination=cont[2])
             self.save_to_elastic(doc_to_save=doc)
 
     @classmethod
     def create_sample_index(cls):
         try:
             logger.info('Check index existence')
-            indices = list(cls.es.indices.get_alias().keys())
+            indices: list = list(cls.es.indices.get_alias().keys())
             if 'content_index' not in indices:
                 cls.es.indices.create(index='content_index')
         except Exception as ex:
@@ -58,8 +60,8 @@ class ESOperations:
     @classmethod
     def save_to_elastic(cls, doc_to_save: ContentElement):
         try:
-            doc = asdict(doc_to_save)
-            res = cls.es.index(index='content_index', document=doc)
+            doc: dict = asdict(doc_to_save)
+            res: Dict[str, Any] = cls.es.index(index='content_index', document=doc)
             return res
         except Exception as ex:
             logger.error(f'Error while saving to Elastic. Saving {doc_to_save} failed.')
@@ -69,4 +71,3 @@ class ESOperations:
 
 if __name__ == '__main__':
     eso = ESOperations()
-    eso.make_and_fill_sample_index()
